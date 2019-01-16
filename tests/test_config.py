d2881f05a14c016fc187d10ddae1a94ad664c0dc
Wang Kai <kaiwkx@gmail.com>
2019-01-17 01:07:30 +0800

    Add config test

---
+++ tests/test_config.py
@@ -0,0 +1,15 @@
+from messsocks.config import get_config
+
+def test_config():
+    config = get_config()
+    server_host = config['server']['host']
+    server_port = config['server']['port']
+
+    assert server_host == '127.0.0.1'
+    assert server_port == '45678'
+
+    client_host = config['client']['host']
+    client_port = config['client']['port']
+
+    assert client_host == '127.0.0.1'
+    assert client_port == '1081'
